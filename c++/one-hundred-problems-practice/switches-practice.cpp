52cb4b5360310760b11ba09ed10110a1206d4e6a
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-08-22 16:37:44 +0900

    not solved switches

--- c++/one-hundred-problems-practice/switches-practice.cpp
+++ c++/one-hundred-problems-practice/switches-practice.cpp
@@ -8,7 +8,7 @@ int main() {
   ll n, m;
   cin >> n >> m;
   vector<ll> k(m);
-  vector<vector<ll>> s;
+  vector<vector<ll>> s(m, vector<ll>(n));;
   for (int i = 0; i < m; ++i) {
     cin >> k.at(i);
     for (int j = 0; j < k.at(i); ++j) cin >> s[i][j];
@@ -16,4 +16,16 @@ int main() {
   vector<ll> p(m);
   for (int i = 0; i < m; i++) cin >> p[i];
   int ans = 0;
+  for (int bit = 0; bit < (1 << n); ++bit) {
+    int tmp = 0;
+    for (int i = 0; i < m; ++i) {
+      int cnt = 0;
+      for (int j = 0; j < k[i]; ++j) {
+        if (bit & (1 << (s[i][j] - 1))) ++cnt;
+      }
+      if (cnt % 2 == p[i]) ++tmp;
+    }
+    if(tmp == m) ++ans;
+  }
+  cout << ans << endl;
 }
--- c++/one-hundred-problems/switches.cpp
+++ c++/one-hundred-problems/switches.cpp
@@ -3,6 +3,7 @@
 #include<vector>
 
 using namespace std;
+typedef long long ll;
 
 int main() {
   int N, M;
@@ -49,6 +50,33 @@ int main() {
     if (tmp == M) {
       ans++;
     }
+  }
+  cout << ans << endl;
+}
+
+
+int answer() {
+  ll n, m;
+  cin >> n >> m;
+  vector<ll> k(m);
+  vector<vector<ll>> s(m, vector<ll>(n));;
+  for (int i = 0; i < m; ++i) {
+    cin >> k.at(i);
+    for (int j = 0; j < k.at(i); ++j) cin >> s[i][j];
+  }
+  vector<ll> p(m);
+  for (int i = 0; i < m; i++) cin >> p[i];
+  int ans = 0;
+  for (int bit = 0; bit < (1 << n); bit++) {
+    int tmp = 0;
+    for (int i = 0; i < m; i++) {
+      int cnt = 0;
+      for (int j = 0; j < k[i]; j++) {
+        if (bit & (1 << (s[i][j] - 1))) ++cnt;
+      }
+      if (cnt % 2 == p[i]) ++tmp;
+    }
+    if (tmp == m) ++ans;
   }
   cout << ans << endl;
 }
