0d41deac323055a3a495e0917078573497d3f9b7
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-08-25 00:17:40 +0900

    update

--- c++/one-hundred-problems-practice/average-length-practice.cpp
+++ c++/one-hundred-problems-practice/average-length-practice.cpp
@@ -1,4 +1,31 @@
-//
-// Created by 仁和活貴 on 2021/08/24.
-//
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <iomanip>
 
+using namespace std;
+typedef long long ll;
+
+vector<int> x(10);
+vector<int> y(10);
+
+double dist(int i, int j) {
+  int dx = x.at(i) - x.at(j);
+  int dy = y.at(i) - y.at(j);
+  return pow(dx * dx + dy * dy, 0.5);
+}
+
+int main() {
+  int n;
+  cin >> n;
+  for (int i = 1; i <= n; i++) cin >> x.at(i) >> y.at(i);
+  double sum = 0.0;
+  vector<int> v(n);
+  for (int i = 0; i < n; i++) v.at(i) = i + 1;
+  do {
+    for (int i = 0; i < n - 1; i++) sum += dist(v.at(i), v.at(i + 1));
+  } while (next_permutation(v.begin(), v.end()));
+  int Factorial = 1;
+  for (int i = 2; i <= n; i++) Factorial *= i;
+  cout << fixed << setprecision(10) << sum / Factorial << endl;
+}
--- c++/one-hundred-problems/average-length.cpp
+++ c++/one-hundred-problems/average-length.cpp
@@ -6,26 +6,46 @@
 using namespace std;
 typedef long long ll;
 
-int N;
-int x[10], y[10];
+vector<int> x(10);
+vector<int> y(10);
 
-// 町と町の間の経路の長さを返す
 double dist(int i, int j) {
-  double dx = x[i] - x[j];
-  double dy = y[i] - y[j];
+  int dx = x.at(i) - x.at(j);
+  int dy = y.at(i) - y.at(j);
   return pow(dx * dx + dy * dy, 0.5);
 }
 
 int main() {
-  cin >> N;
-  for (int i = 1; i <= N; i++) cin >> x[i] >> y[i];
+  int n;
+  cin >> n;
+  // x = {1: ?, 2: ?, ..., n: ?}
+  // y = {1: ?, 2: ?, ..., n: ?}
+  for (int i = 1; i <= n; i++) cin >> x.at(i) >> y.at(i);
   double sum = 0.0;
-  vector<int> v(N);
-  for (int i = 0; i < N; i++) v[i] = i + 1;
+  vector<int> v(n);
+  for (int i = 0; i < n; i++) v.at(i) = i + 1;
+  // v = {0: 1, 1: 2, ... , n-1: n}
   do {
-    for (int i = 0; i < N - 1; i++) sum += dist(v[i], v[i + 1]);
+    // i = {0, 1, ..., n-2}
+    // sum = dist(1, 2) + dist(2, 3) + ... + dist(n-1, n)
+    // 12
+    // 23
+    // 13
+    // 32
+    // 21
+    // 13
+    // 23
+    // 31
+    // 31
+    // 12
+    // 32
+    // 21
+    for (int i = 0; i < n - 1; i++) {
+      cout << v.at(i) << v.at(i + 1) << endl;
+      sum += dist(v.at(i), v.at(i + 1));
+    }
   } while (next_permutation(v.begin(), v.end()));
   int Factorial = 1;
-  for (int i = 2; i <= N; i++) Factorial *= i;
+  for (int i = 2; i <= n; i++) Factorial *= i;
   cout << fixed << setprecision(10) << sum / Factorial << endl;
 }
