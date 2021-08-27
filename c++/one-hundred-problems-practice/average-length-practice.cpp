2caa824f4f56efa50a80f345d4ae8a584c1fb067
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-08-27 23:58:50 +0900

    update

--- c++/one-hundred-problems-practice/average-length-practice.cpp
+++ c++/one-hundred-problems-practice/average-length-practice.cpp
@@ -6,10 +6,28 @@
 using namespace std;
 typedef long long ll;
 
+vector<int> x(10);
+vector<int> y(10);
+
+double dist(int s, int t) {
+  int dx = x.at(s) - x.at(t);
+  int dy = y.at(s) - y.at(t);
+  return pow(dx * dx + dy * dy, 0.5);
+}
+
 int main() {
   int n;
   cin >> n;
-  vector<int> x(10);
-  vector<int> y(10);
   for (int i = 1; i <= n; i++) cin >> x.at(i) >> y.at(i);
+  double sum = 0.0;
+  vector<int> v(n);
+  for (int i = 0; i < n; i++) v.at(i) = i + 1;
+  do {
+    for (int i = 0; i < n - 1; i++) {
+      sum += dist(v.at(i), v.at(i + 1));
+    }
+  } while (next_permutation(v.begin(), v.end()));
+  int Factorial = 1;
+  for (int i = 2; i <= n; i++) Factorial *= i;
+  cout << fixed << setprecision(10) << sum / Factorial << endl;
 }
