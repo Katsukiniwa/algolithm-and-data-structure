2d451460ab98b8374cb696adb1e31c260c749d47
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-11-02 09:42:46 +0900

    update

---
+++ c++/LeetCode/two-sum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  int target;
+  vector<int> A(100000);
+
+  for (int i = 0; i < A.size() - 1; i++) {
+    for (int j = 0; j < A.size(); j++) {
+      if (A[i] + A[j] == target) {
+        cout << "[i, j]" << endl;
+      }
+    }
+  }
+}
