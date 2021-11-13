9b603570079ab43cf129c6d4a14836c65d62e6b6
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-11-13 11:15:45 +0900

    update

---
+++ c++/atcoder-problems/medium/trained-practice.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+  int N;
+  cin >> N;
+  vector<int> a(N);
+  for (int k = 0; k < N; ++k) cin >> a.at(k);
+}
