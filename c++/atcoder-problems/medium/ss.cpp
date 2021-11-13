9c5813068c31da1c00d92f0fd31e8308431244b0
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-11-13 17:57:18 +0900

    initialize ss

---
+++ c++/atcoder-problems/medium/ss.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+  string s;
+  string k, j;
+  int o = 0;
+  cin >> s;
+
+  // { 1, 2, 3, 4 }
+
+  cout << o << endl;
+}
