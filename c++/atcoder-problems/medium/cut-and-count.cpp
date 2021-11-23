be316b8b302528bde68ff8b6ee1b6f3dd9fd75f3
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-11-24 00:16:34 +0900

    update

--- c++/atcoder-problems/medium/cut-and-count.cpp
+++ c++/atcoder-problems/medium/cut-and-count.cpp
@@ -7,18 +7,24 @@ using namespace std;
 using ll = long long;
 
 int main() {
-  int N, m = 0;
+  int N, result = 0;
   string S;
   cin >> N >> S;
   for (int i = 1; i < S.size() - 1; i++) {
     string X = S.substr(0, i), Y = S.substr(i);
-    int c = 0;
+    int tmp = 0;
     for (char z = 'a'; z <= 'z'; z++) {
+      /**
+       * XとYの両方に対象のアルファベットが含まれているかどうかをチェック
+       */
       if (count(X.begin(), X.end(), z) * count(Y.begin(), Y.end(), z) != 0) {
-        c++;
+        tmp++;
       }
     }
-    m = max(c, m);
+    /**
+     * 現在のresultよりtmpが大きかった場合のみresultを更新する
+     */
+    result = max(tmp, result);
   }
-  cout << m << endl;
+  cout << result << endl;
 }
