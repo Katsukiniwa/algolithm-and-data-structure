c45f50ad05ce0add242d30e70a2d4e8768fa90ec
Katsukiniwa <katsukiniwa0217@gmail.com>
2022-01-17 08:35:49 +0900

    merge-sorted-array.cpp

---
+++ c++/LeetCode/easy/merge-sorted-array.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  vector<int> nums1, nums2;
+  int m, n;
+  int i = m - 1, j = n - 1, k = m + n - 1;
+  while (i >= 0 && j >= 0) {
+    if (nums1[i] > nums2[j]) {
+      nums1[k] = nums1[i];
+      i--;
+      k--;
+    } else {
+      nums1[k] = nums2[j];
+      j--;
+      k--;
+    }
+  }
+  while (i >= 0)
+    nums1[k--] = nums1[i--];
+  while (j >= 0)
+    nums1[k--] = nums2[j--];
+}
