b7f8e2c7705e54ea5de68377ed2df5bf494841e4
Katsukiniwa <katsukiniwa0217@gmail.com>
2022-02-01 19:15:01 +0900

    prison.cpp

--- c++/atcoder-problems/easy/coins.cpp
+++ c++/atcoder-problems/easy/coins.cpp
@@ -10,7 +10,6 @@ int main() {
   for (int i = 0; i <= A; ++i) {
     for (int j = 0; j <= B; ++j) {
       for (int k = 0; k <= C; ++k) {
-//        cout << "A: " << A << " " << "B: " << B << " " << "C: " << C << " " << "sum: " << (A * 500) + (B * 100) + (C * 50) << endl;
         if ((i * 500) + (j * 100) + (k * 50) == X) ++ res;
       }
     }
