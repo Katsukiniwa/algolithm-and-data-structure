ecb52c01d978821d3c755b3006527d7a4e1673b7
Katsukiniwa <katsukiniwa0217@gmail.com>
2022-02-05 23:12:36 +0900

    attack-survival.cpp

--- c++/atcoder-problems/easy/attack-survival.cpp
+++ c++/atcoder-problems/easy/attack-survival.cpp
@@ -4,32 +4,19 @@
 using namespace std;
 
 int main() {
-  int n, k, q;
-  cin >> n >> k >> q;
-  vector<int> a(q);
-  vector<int> vn(n);
-  for (int & i : a) {
-    cin >> i;
+  int N, K, Q;
+  cin >> N >> K >> Q;
+  vector<int> A(Q), point(N, K - Q);
+  for (int i = 0; i < Q; ++i) {
+    int tmp;
+    cin >> tmp;
+    ++point.at(tmp - 1);
   }
-
-  /**
-   * 開始時点での全員の得点をk-qとする
-   */
-  for (int & i : vn) {
-    i = k - q;
-  }
-  /**
-   * 正解者の得点を1増やす
-   */
-  for (int i : a) {
-    vn.at(i - 1)++;
-  }
-
-  for (int i : vn) {
-    if (i <= 0) {
-      cout << "No" << endl;
-    } else {
+  for (int i : point) {
+    if (i > 0) {
       cout << "Yes" << endl;
+    } else {
+      cout << "No" << endl;
     }
   }
 }
