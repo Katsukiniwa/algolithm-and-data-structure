5efd8a1ed0946120bcedb9bc113c16757bbf7939
Katsukiniwa <katsukiniwa0217@gmail.com>
2024-02-22 09:11:20 +0900

    chore: update

--- BootCampForBeginners/Medium/abc-065-b.cpp
+++ BootCampForBeginners/Medium/abc-065-b.cpp
@@ -1,27 +1,23 @@
 #include<iostream>
-#include<string>
-#include<algorithm>
-#include<vector>
-#include<math.h>
-#include<map>
-#include<set> 
-#include<utility>
 using namespace std;
 
-int main(){
+int main() {
   int N;
-  cin>>N;
-  vector<int>A(N);
-  for(int i = 0; i < N; i++) cin >> A[i];
-  int b=1,counter=0;
+  cin >> N;
+  int a[N];
   for(int i = 0; i < N; i++) {
-      b=A[b-1];
-      counter++;
-      if(b==2) {
-          cout<<counter<<endl; 
-          return 0;
-      }
+    cin >> a[i];
   }
-  cout<<-1<<endl;
-  return 0; 
+
+  int index = 1, result = 0;
+  for(int i = 0; i < N; i++) {
+    index = a[index - 1];
+    if (index == 2) {
+      cout << result + 1 << endl;
+      return 0;
+    }
+    result++;
+  }
+  
+  cout << -1 << endl;
 }
